7b7351c1ed38dea8a90a126c2915173c1c96631b
TianCastroUnill <153392749+TianCastroUnill@users.noreply.github.com>
2025-02-18 10:50:45 -0500

    Update CursosInscritos.java
    Solo se crearon lo que van ah ser las funciones, no se creo logica

--- AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/procesos/CursosInscritos.java
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/procesos/CursosInscritos.java
@@ -4,10 +4,38 @@
  */
 package com.mycompany.aplicacion_de_conceptos.procesos;
 
+import com.mycompany.aplicacion_de_conceptos.entidades.Inscripcion;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Estudiante_MCA
  */
 public class CursosInscritos {
+    private List<Inscripcion> listado = new ArrayList<>();
     
+    public void inscribirCurso(Inscripcion inscripcion){
+        listado.add(inscripcion);
+    }
+    
+    public void  eliminar(Inscripcion inscripcion){
+        //falta logica
+    }
+    
+    public void actualizar(Inscripcion inscripcion){
+        //falta logica
+    }
+    
+    public void guardarInformacion(){
+        //falta logica
+    }
+    
+//    public List toString(){
+//        Falta logica, se deja comentado por error
+//    }
+    
+    public void cargarDatos(){
+        //falta logica
+    }
 }
