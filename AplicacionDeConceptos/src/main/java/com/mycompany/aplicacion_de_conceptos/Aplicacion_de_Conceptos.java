8b6feac2c837fcd5077ecea05a3377a0f84ea520
TianCastroUnill <153392749+TianCastroUnill@users.noreply.github.com>
2025-02-18 08:28:20 -0500

    Add files via upload
    Version temprana

---
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/Aplicacion_de_Conceptos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplicacion_de_conceptos;
+
+/**
+ *
+ * @author Estudiante_MCA
+ */
+public class Aplicacion_de_Conceptos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
