469c53f44913099cc1fe7e871cac78bee075d873
DavidRamos125 <fdramos@unillanos.edu.co>
2025-02-18 11:11:28 -0500

    Update CursoProfesor.java

--- AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/procesos/CursoProfesor.java
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/procesos/CursoProfesor.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.aplicacion_de_conceptos.procesos;
 
 import com.mycompany.aplicacion_de_conceptos.entidades.Profesor;
