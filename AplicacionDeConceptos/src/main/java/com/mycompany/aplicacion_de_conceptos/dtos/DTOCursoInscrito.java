8bac25456cdfce44910d9689870facd5ef124c1c
david <fdramos@unillanos.edu.co>
2025-03-03 22:52:00 -0500

    creando servicios...

--- AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/dtos/DTOCursoInscrito.java
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/dtos/DTOCursoInscrito.java
@@ -1,8 +1,49 @@
 package com.mycompany.aplicacion_de_conceptos.dtos;
 
-import com.mycompany.aplicacion_de_conceptos.persistencia.CRUD;
-import com.mycompany.aplicacion_de_conceptos.procesos.CursosInscritos;
-
 public class DTOCursoInscrito {
-    private CRUD<CursosInscritos> crud;
+    private DTOCurso curso;
+    private int año;
+    private int semestre;
+    private DTOEstudiante estudiante;
+
+    // Constructor
+    public DTOCursoInscrito(DTOCurso curso, int año, int semestre, DTOEstudiante estudiante) {
+        this.curso = curso;
+        this.año = año;
+        this.semestre = semestre;
+        this.estudiante = estudiante;
+    }
+
+    // Getters y Setters
+    public DTOCurso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(DTOCurso curso) {
+        this.curso = curso;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public DTOEstudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(DTOEstudiante estudiante) {
+        this.estudiante = estudiante;
+    }
 }
