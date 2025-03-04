b2f57afa996003fb741126d0b18da5f556cc5d97
david <fdramos@unillanos.edu.co>
2025-03-03 22:37:06 -0500

    creando servicios...

--- AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/servicios/ServicioCursoProfesor.java
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/servicios/ServicioCursoProfesor.java
@@ -2,11 +2,13 @@ package com.mycompany.aplicacion_de_conceptos.servicios;
 
 import com.mycompany.aplicacion_de_conceptos.entidades.CursoProfesor;
 import com.mycompany.aplicacion_de_conceptos.persistencia.CRUD;
+import com.mycompany.aplicacion_de_conceptos.persistencia.baseDatos.DBCursoProfesor;
 
 public class ServicioCursoProfesor {
     private CRUD<CursoProfesor> crud;
-    public ServicioCursoProfesor(CRUD<CursoProfesor> crud) {
-        this.crud = crud;
+
+    public ServicioCursoProfesor() {
+        this.crud = new DBCursoProfesor();
     }
 
 
--- AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/servicios/ServicioInscripcion.java
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/servicios/ServicioInscripcion.java
@@ -2,12 +2,13 @@ package com.mycompany.aplicacion_de_conceptos.servicios;
 
 import com.mycompany.aplicacion_de_conceptos.entidades.Inscripcion;
 import com.mycompany.aplicacion_de_conceptos.persistencia.CRUD;
+import com.mycompany.aplicacion_de_conceptos.persistencia.baseDatos.DBCursoInscrito;
 
 public class ServicioInscripcion {
     private CRUD<Inscripcion> crud;
 
-    public ServicioInscripcion(CRUD<Inscripcion> crud) {
-        this.crud = crud;
+    public ServicioInscripcion() {
+        this.crud = new DBCursoInscrito();
     }
 
 }
