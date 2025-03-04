eea498b92a5f20c0455cc6d8b17471260fa9b8a9
david <fdramos@unillanos.edu.co>
2025-03-03 22:14:52 -0500

    Modificacion ServicioPersona

--- AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/servicios/ServicioPersona.java
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/servicios/ServicioPersona.java
@@ -3,16 +3,20 @@ package com.mycompany.aplicacion_de_conceptos.servicios;
 import com.mycompany.aplicacion_de_conceptos.dtos.DTOPersona;
 import com.mycompany.aplicacion_de_conceptos.entidades.Persona;
 import com.mycompany.aplicacion_de_conceptos.persistencia.CRUD;
+import com.mycompany.aplicacion_de_conceptos.persistencia.baseDatos.DBPersona;
 
 public class ServicioPersona {
     private CRUD<Persona> crud;
 
-    public ServicioPersona(CRUD<Persona> crud) {
-        this.crud = crud;
+    public ServicioPersona() {
+
     }
 
     public void crearPersona(DTOPersona dtoPersona) {
         Persona persona = new Persona(dtoPersona.getId(), dtoPersona.getNombres(), dtoPersona.getApellidos(), dtoPersona.getEmail());
+        crud = new DBPersona();
+        crud.crear(persona);
+        crud = new DBPersona();
         crud.crear(persona);
     }
 }
