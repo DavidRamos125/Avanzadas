4f918874153c2ed0124be227ffdd3a300a0a20b2
david <fdramos@unillanos.edu.co>
2025-03-10 19:51:53 -0500

    añadiendo servicios

--- AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/procesos/ServicioPrograma.java
+++ AplicacionDeConceptos/src/main/java/com/mycompany/aplicacion_de_conceptos/procesos/ServicioPrograma.java
@@ -1,13 +1,20 @@
 package com.mycompany.aplicacion_de_conceptos.procesos;
 
+import com.mycompany.aplicacion_de_conceptos.dtos.DTOPrograma;
 import com.mycompany.aplicacion_de_conceptos.entidades.Programa;
 import com.mycompany.aplicacion_de_conceptos.persistencia.CRUD;
 import com.mycompany.aplicacion_de_conceptos.persistencia.baseDatos.DBPrograma;
+
+import java.util.List;
 
 public class ServicioPrograma {
     private CRUD<Programa> crud;
 
     public ServicioPrograma() {
         this.crud = new DBPrograma();
+    }
+
+    public List<DTOPrograma> obtenerTodos(){
+        return List.of();
     }
 }
